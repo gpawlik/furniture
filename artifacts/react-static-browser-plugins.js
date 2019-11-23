ff6f438dae684c792fd57c8c3908e4e58c5d3507
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2019-11-23 09:29:38 +0000

    Initial commit

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,27 @@
+// Imports
+import plugin0 from '/Users/grzegorzpawlik/DEV/GP/furniture/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "/Users/grzegorzpawlik/DEV/GP/furniture/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/grzegorzpawlik/DEV/GP/furniture/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "/Users/grzegorzpawlik/DEV/GP/furniture/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/grzegorzpawlik/DEV/GP/furniture",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/Users/grzegorzpawlik/DEV/GP/furniture/src/pages/404.js': require('/Users/grzegorzpawlik/DEV/GP/furniture/src/pages/404.js').default,
+'/Users/grzegorzpawlik/DEV/GP/furniture/src/pages/about.js': require('/Users/grzegorzpawlik/DEV/GP/furniture/src/pages/about.js').default,
+'/Users/grzegorzpawlik/DEV/GP/furniture/src/pages/blog.js': require('/Users/grzegorzpawlik/DEV/GP/furniture/src/pages/blog.js').default,
+'/Users/grzegorzpawlik/DEV/GP/furniture/src/pages/index.js': require('/Users/grzegorzpawlik/DEV/GP/furniture/src/pages/index.js').default,
+'/Users/grzegorzpawlik/DEV/GP/furniture/src/containers/Post': require('/Users/grzegorzpawlik/DEV/GP/furniture/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/Users/grzegorzpawlik/DEV/GP/furniture/src/pages/404.js'
+
